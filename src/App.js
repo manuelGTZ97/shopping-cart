51348591ffe61201ff321df869d9777b677c8eb4
manuel <manuel.gutierrez@lifebeacon.com>
2022-02-18 15:44:31 -0600

    Add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
+// 1.- Products table (Product description, Price, Quantity and Add to cart button)
+// 2.- Shopping cart (Product description, Price and Remove from cart button)
+// 3.- Total price
+
 function App() {
-  return <div className="App">Hi!</div>;
+  return (
+    <div className="App">
+      <header className="shoping-header">
+        <h1>Shopping Cart</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
