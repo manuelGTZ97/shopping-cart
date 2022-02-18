eef018660b02c28d423fe6781f0b6cf7ccbb4741
manuel <manuel.gutierrez@lifebeacon.com>
2022-02-18 16:20:54 -0600

    Html structure

--- src/App.js
+++ src/App.js
@@ -10,6 +10,65 @@ function App() {
       <header className="shoping-header">
         <h1>Shopping Cart</h1>
       </header>
+      <div className="shoping-products">
+        <table>
+          <thead>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Add to cart</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Chicken</td>
+              <td>$10</td>
+              <td>2</td>
+              <td>
+                <button>Add to cart</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Beef</td>
+              <td>$20</td>
+              <td>3</td>
+              <td>
+                <button>Add to cart</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Tomato</td>
+              <td>$10</td>
+              <td>10</td>
+              <td>
+                <button>Add to cart</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="shoping-cart">
+        <table>
+          <thead>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Remove from cart</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Empty</td>
+              <td>0</td>
+              <td>0</td>
+              <td>
+                <button>Remove from cart</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="shoping-total">
+        <h2>Total: $0</h2>
+      </div>
     </div>
   );
 }
