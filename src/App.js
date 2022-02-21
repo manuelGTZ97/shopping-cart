5b6c69839b5c1ae241fbf893595d1a915baa4a1c
Ramgarok <ramfaga2@gmail.com>
2022-02-21 14:26:30 -0600

    onClick Method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
-import { useState } from "react";
 import {
   Header,
   ShoppingCart,
   ShoppingProducts,
   ShoppingTotal,
 } from "./components";
+import { useState } from "react";
 
-// 1.- Dinmic Products table (Product description, Price, Quantity and Add to cart button)
+// 1.- Products table (Product description, Price, Quantity and Add to cart button)
 // 2.- Shopping cart (Product description, Price and Remove from cart button)
 // 3.- Total price
+
+
 
 function App() {
   const [products, setProducts] = useState([
@@ -30,10 +32,15 @@ function App() {
     },
   ]);
 
+  const oncClickAddCart = (products) => {
+    setProducts(products.info)
+
+  }
+
   return (
     <div className="App">
       <Header />
-      <ShoppingProducts products={products} />
+      <ShoppingProducts products={products} oncClickAddCart={oncClickAddCart}/>
       <ShoppingCart />
       <ShoppingTotal />
     </div>
--- src/components/ShoppingProducts.js
+++ src/components/ShoppingProducts.js
@@ -1,28 +1,60 @@
-function ShoppingProducts({ products }) {
-  return (
-    <div className="shoping-products">
+function ShoppingProducts({products}){
+
+  const onClick = () =>{
+    console.log(products)
+  }
+  
+  
+    return (
+        <div className="shopping-products">
       <table>
-        <thead>
-          <th>Product</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Add to cart</th>
-        </thead>
-        <tbody>
+          <thead>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Add to cart</th>
+          </thead>
+          <tbody>
           {products.map((product) => (
             <tr>
               <td>{product.name}</td>
-              <td>${product.price}</td>
+              <td>{product.price}</td>
               <td>{product.quantity}</td>
+              <td>
+                <button onClick={onClick}>Add to cart</button>               
+              </td>
+            </tr>
+          ))}
+          </tbody>
+      </table>
+      <table>
+          <thead>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Add to cart</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Chicken</td>
+              <td>$10</td>
+              <td>2</td>
               <td>
                 <button>Add to cart</button>
               </td>
             </tr>
-          ))}
-        </tbody>
+            <tr>
+              <td>Empty</td>
+              <td>0</td>
+              <td>0</td>
+              <td>
+                <button>Remove from cart</button>
+              </td>
+            </tr>
+          </tbody>
       </table>
-    </div>
-  );
+      </div>
+    )
 }
-
+//tarea: 
 export default ShoppingProducts;
